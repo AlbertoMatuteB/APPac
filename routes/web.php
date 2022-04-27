212b8cd0e205439c382eafa06f3fabe69971051d
franciscodzv <franciscodzv@gmail.com>
2022-04-26 23:48:27 -0500

    feat(backend): :lock: Implemented RBAC completely

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function ()
+{
+    return view('home');
+});
+
+
+Route::get('/', function ()
+{
+    return view('auth.login');
 });
