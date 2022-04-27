d661ffc94483f641c11e504c08bd625da3b476bf
Sebastian Resendiz <sebas.r.2599@gmail.com>
2022-04-26 22:12:19 -0500

    tailwind styles for temp home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="grid grid-cols-1 grid-rows-1 items-center bg-slate-700 w-100 h-screen">
+        <h1 class="text-3xl text-white text-center align-middle">
+            Welcome to appac
+        </h1>
+    </div>
+
+</body>
+
+</html>
