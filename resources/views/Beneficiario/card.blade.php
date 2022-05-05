2a41f8305b5da4d02cf8ffcbe40fe3a63072f488
MIguel Angel Marines Olvera <A01705317@itesm.mx>
2022-05-05 09:04:44 -0500

    Tailwind listo para dar formato al front.

--- resources/views/Beneficiario/card.blade.php
+++ resources/views/Beneficiario/card.blade.php
@@ -13,8 +13,6 @@
     
 @endif
 
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"/>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet"> -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
