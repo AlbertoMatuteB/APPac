6a6ffff9a9e923114c4d3116b4e0e3b993976139
AlbertoMatuteB <elmatu117@gmail.com>
2022-05-06 13:46:26 -0500

    feat(migrations): :construction: Primera version de las migraciones

--- database/migrations/2022_05_06_181428_create_institution_table.php
+++ database/migrations/2022_05_06_181428_create_institution_table.php
@@ -13,16 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('institutions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('last_name');
-            $table->foreignId('rol_id')->constrained();
-            $table->string('email')->unique();
-            //$table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('visibility');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('institution');
     }
 };
--- database/migrations/2022_05_06_182241_create_evaluation_table.php
+++ database/migrations/2022_05_06_182241_create_evaluation_table.php
@@ -13,16 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('last_name');
-            $table->foreignId('rol_id')->constrained();
-            $table->string('email')->unique();
-            //$table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('visibility');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('beneficiary_id')->constrained();
+            $table->date('date');
+            $table->text('observations');
             $table->timestamps();
         });
     }
@@ -34,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('beneficiary_evaluation');
     }
 };
