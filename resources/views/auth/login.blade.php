41aa49f80f1100fda1ac5e6d1799a855e7f3697b
Sebastian Resendiz <sebas.r.2599@gmail.com>
2022-05-08 20:40:02 -0500

    Login UI terminado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,63 +2,65 @@
 
 @section('content')
 
-<div class="h-full">
+<div class="h-screen">
     <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div class="max-w-md w-full space-y-8">
+        <div class="bg-white max-w-xl w-full space-y-10 px-14 py-20 rounded-lg shadow-md">
             <div>
-                <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt="Workflow">
-                <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-                <p class="mt-2 text-center text-sm text-gray-600">
-                    Or
-                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial
-                    </a>
-                </p>
+                <h2 class="font-blue-appac text-center text-xl font-semibold">Bienvenido</h2>
             </div>
-            <form class="mt-8 space-y-6" action="#" method="POST">
+            <form class="mt-8 space-y-6" method="POST" action="{{ route('login') }}">
+                @csrf
                 <input type="hidden" name="remember" value="true">
-                <div class="rounded-md shadow-sm -space-y-px">
-                    <div>
-                        <label for="email-address" class="sr-only">Email address</label>
-                        <input id="email-address" name="email" type="email" autocomplete="email" required
-                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                            placeholder="Email address">
-                    </div>
-                    <div>
-                        <label for="password" class="sr-only">Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required
-                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                            placeholder="Password">
-                    </div>
+
+                <div class="relative flex w-full flex-wrap items-stretch">
+                    <span
+                        class="z-10 h-full leading-snug font-normal absolute text-center text-slate-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-2 py-3">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+                        </svg>
+                    </span>
+                    <input id="email" name="email" type="email" placeholder="Correo electronico"
+                        value="{{ old('email') }}" required autocomplete="email"
+                        class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-transparent text-base border-b-2  outline-none focus:border-black w-full pl-10" />
+                    @error('email')
+                    <span class="text-red-500" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="relative flex w-full flex-wrap items-stretch">
+                    <span
+                        class="z-10 h-full leading-snug font-normal absolute text-center text-slate-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-2 py-3">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z">
+                            </path>
+                        </svg>
+                    </span>
+                    <input id="password" name="password" type="password" placeholder="Contraseña"
+                        class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-transparent text-base border-b-2  outline-none focus:border-black w-full pl-10" />
+                    @error('password')
+                    <span class="text-red-500" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
 
-                <div class="flex items-center justify-between">
+                <div class="flex items-center justify-center">
                     <div class="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox"
-                            class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-                        <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
-                    </div>
-
-                    <div class="text-sm">
-                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password?
-                        </a>
+                        <input id="remember-me" name="remember" type="checkbox" id="remember" {{ old('remember') ? 'checked' : '' }}
+                            class="h-4 w-4 text-slate-300 focus:ring-indigo-500 border-slate-300 rounded">
+                        <label for="remember" class="ml-2 block text-sm text-slate-300">{{ __('Remember Me') }}</label>
                     </div>
                 </div>
 
                 <div>
                     <button type="submit"
-                        class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                            <!-- Heroicon name: solid/lock-closed -->
-                            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </span>
-                        Sign in
+                        class="group relative w-full flex justify-center p-4 text-xl font-bold rounded-lg text-white bg-blue-appac hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800">
+                        {{ __('Login') }}
                     </button>
                 </div>
             </form>
