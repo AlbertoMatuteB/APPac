be30a911f9c2e5a43025c880ff72232462e2b42f
Emilio Rivas <miles.fra.2000@gmail.com>
2022-05-15 18:08:11 -0500

    fix: :zap: mejora
    staging inicial

--- resources/views/usuarios/consultarUsuarios.blade.php
+++ resources/views/usuarios/consultarUsuarios.blade.php
@@ -8,48 +8,36 @@
             <div>
                 <h1 class="font-blue-appac text-left text-4xl font-black">Usuarios</h2>
             </div>
-            <div
-                class="bg-default-grey w-full rounded-xl grid grid-rows-2 grid-cols-4 gap-x-6 gap-y-2 items-center py-4 pr-10">
-                <div class="font-semibold pl-4">Buscar usuario</div>
-                <div class="font-semibold">Buscar por correo</div>
-                <div class="font-semibold pl-4">Buscar por rol</div>
-                <div>&nbsp;</div>
-                <div class="pl-4">
-                    <span
-                        class="z-10 h-full leading-snug font-normal absolute text-center text-slate-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-2 py-1">
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg>
-                    </span>
-                    <input type="text" placeholder="Buscar..."
-                        class="bg-white px-3 py-1 placeholder-slate-400 text-slate-600 relative text-base  border-2 rounded-2xl outline-none focus:border-slate-300 w-full pl-8" />
+            <div class="bg-default-grey w-full rounded-xl grid  grid-rows-2 grid-cols-3 gap-x-6 gap-y-2 items-center py-4 pr-10">
+                
+                <div class="flex flex-column">
+                    <div class="font-semibold pl-4">Buscar por rol</div>
                 </div>
-                <div> <span
-                        class="z-10 h-full leading-snug font-normal absolute text-center text-slate-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-2 py-1">
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg>
-                    </span>
-                    <input type="text" placeholder="Buscar..."
-                        class="bg-white px-3 py-1 placeholder-slate-400 text-slate-600 relative text-base  border-2 rounded-2xl outline-none focus:border-slate-300 w-full pl-8" />
-                </div>
-                <div>
-                    <select
-                        class="block appearance-none w-full bg-white border-gray-200 placeholder-slate-400 text-slate-600 py-2 px-3 pr-8  border-2 rounded-2xl leading-tight focus:outline-none focus:border-slate-300">
-                        <option>Administrador</option>
-                        <option>Usuario base</option>
-                    </select>
-                </div>
-                <div>
-                    <button class="w-full rounded-lg bg-blue-appac text-white text-center py-2 px-24">
-                        <a class="text-white text-center" href="#"
-                            target="_blank" rel="">Agregar</a>
-                    </button>
-                </div>
+                
+                 
+                <div class="flex flex-row">
+                    <form method="POST" action="/crearUsuario" class="pl-4  items-center">
+                    @csrf 
+                        <div >
+                            <select
+                                class="block appearance-none w-full bg-white border-gray-200 placeholder-slate-400 text-slate-600 py-2 px-4 pr-8  border-2 rounded-2xl leading-tight focus:outline-none focus:border-slate-300">
+                                <option>Administrador</option>
+                                <option>Usuario base</option>
+                            </select>
+                        </div>
+                        <div>
+                        <button type="submit" class="w-full rounded-lg bg-blue-appac text-white text-center py-2 px-24">
+                        <a class="text-white text-center">Buscar</a>
+                        </button>
+                                
+                        </div>
+                    </form>
+                 </div>
+
+                <div class="flex flex-row">
+                    <a class="text-white text-center w-full rounded-lg bg-green-appac text-white text-center py-2 px-24" href="#" target="_blank" rel="">Agregar</a>
+                </div>       
+            
             </div>
             <div class="overflow-x-auto">
                 <table class="table-auto w-full">
