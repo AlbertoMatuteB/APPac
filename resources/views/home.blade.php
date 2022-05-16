fba339b4e8dc99d2c51c13b3a53db2edb04c00b0
Emilio Rivas <47706187+RivasEmilio@users.noreply.github.com>
2022-05-15 22:33:40 -0500

    Revert "Feat/frontend/main menu"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="h-full menu-grid content-center justify-center gap-12 px-10 py-10">
-    <a href="/beneficiarios"
-        class="flex flex-col items-center justify-center bg-white w-full h-full rounded-lg shadow-md hover:shadow-lg">
-        <span class="material-icons material-icons-outlined  font-blue-appac menu-icon-size">
-            school
-        </span>
-        <span class="font-blue-appac font-semibold">Beneficiarios</span>
-    </a>
-    <a href="/beneficiarios"
-        class="flex flex-col items-center justify-center bg-white w-full h-full rounded-lg shadow-md hover:shadow-lg">
-        <span class="material-icons material-icons-outlined  font-blue-appac menu-icon-size">
-            leaderboard
-        </span>
-        <span class="font-blue-appac font-semibold">Evaluaciones</span>
-    </a>
-
-    <a href="/beneficiarios"
-        class="flex flex-col items-center justify-center bg-white w-full h-full rounded-lg shadow-md hover:shadow-lg">
-        <span class="material-icons material-icons-outlined  font-blue-appac menu-icon-size">
-            summarize
-        </span>
-        <span class="font-blue-appac font-semibold">Diagnóstico</span>
-    </a>
-
-    <a href="/usuario"
-        class="flex flex-col items-center justify-center bg-white w-full h-full rounded-lg shadow-md hover:shadow-lg">
-        <span class="material-icons material-icons-outlined  font-blue-appac menu-icon-size">
-            group
-        </span>
-        <span class="font-blue-appac font-semibold">Usuarios</span>
-    </a>
+<div class="">
+    <h1 class="text-3xl text-black text-center align-middle">
+        Bienvenido al sistema de evaluaciones de APAC I.A.P Qro!
+    </h1>
 </div>
-
 
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\Beneficiary\ListBeneficiary;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController;
@@ -18,16 +16,17 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/home', HomeController::class)->middleware('auth');
+Route::get('/home', function () {
+    return view('home');
+})->middleware('auth');
 
 
 Route::get('/', function () {
     return view('auth.login');
 });
-
+ 
 Route::get('/usuario', [UserController::class, 'index'])->middleware('auth');
 
 Route::get('/usuario/{id}', [UserController::class, 'getUser'])->middleware('auth');
 
 Route::get('/beneficiarios', ListBeneficiary::class)->middleware('auth');
-
