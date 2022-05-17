f701dc486ef337c0f7e238a28b1c452ec8dc4366
RHazaelJimAr <randy_hazael_10@live.com.mx>
2022-05-16 21:18:40 -0500

    feat: :art: Más correcciones a front

--- resources/views/usuarios/editarUsuario.blade.php
+++ resources/views/usuarios/editarUsuario.blade.php
@@ -123,21 +123,24 @@
 @endif
 @if(Auth::user()->role_id == 1)
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Editar Usuario') }}</div>
+<div class="">
+    {{-- min-h-full --}}
+    <div class="min-h-full flex items-center justify-center py-10 px-16 sm:px-6 lg:px-8">
+        <div class="bg-white w-full space-y-10 px-14 py-8 rounded-lg shadow-md">
+            <div>
+                <div class="font-blue-appac text-left text-4xl font-black">{{ __('Editar Usuario') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ url('editarUsuario', $usuario['id']) }}">
+                    <form class="grid grid-rows-11 gap-y-2 items-center" method="POST" action="{{ url('editarUsuario', $usuario['id']) }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="font-semibold pt-2">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{$usuario['name']}}" required autocomplete="name" autofocus>
+                                <input id="name" type="text"
+                                class="bg-default-grey px-3 py-1 placeholder-slate-400 text-slate-600 relative text-base  border-2 rounded-2xl outline-none focus:border-slate-300 w-full pl-8"
+                                name="name" value="{{$usuario['name']}}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -147,11 +150,13 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="font-semibold pt-2">
                             <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Apellido') }}</label>
 
                             <div class="col-md-6">
-                                <input id="last_name" type="text" class="form-control @error('name') is-invalid @enderror" name="last_name" value="{{$usuario['last_name']}}" required autocomplete="last_name" autofocus>
+                                <input id="last_name" type="text"
+                                class="bg-default-grey px-3 py-1 placeholder-slate-400 text-slate-600 relative text-base  border-2 rounded-2xl outline-none focus:border-slate-300 w-full pl-8"
+                                name="last_name" value="{{$usuario['last_name']}}" required autocomplete="last_name" autofocus>
 
                                 @error('last_name')
                                     <span class="invalid-feedback" role="alert">
@@ -161,11 +166,13 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="font-semibold pt-2">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo Electrónico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{$usuario['email']}}" required autocomplete="email">
+                                <input id="email" type="email"
+                                class="block appearance-none w-full bg-default-grey border-gray-200 placeholder-slate-400 text-slate-600 py-2 px-3 pr-8  border-2 rounded-2xl leading-tight focus:outline-none focus:border-slate-300"
+                                name="email" value="{{$usuario['email']}}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -177,7 +184,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="w-full rounded-lg bg-blue-appac py-2 px-10; text-white text-center">
                                     {{ __('Actualizar') }}
                                 </button>
                             </div>
@@ -186,7 +193,7 @@
                 </div>
             </div>
             <br>
-            <div class="card">
+            <!-- <div class="card">
                 <div class="card-header">Lista de usuarios:</div>
                     <table class="table table-bordered table-sm table-responsive-sm">
                         <thead class="thead-light text-center">
@@ -219,7 +226,7 @@
 
                     </table>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </div>
