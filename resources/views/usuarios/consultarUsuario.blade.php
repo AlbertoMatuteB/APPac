82db1b56cf0985d38616b5677c21dbef77d70d24
Sebastian Resendiz <sebas.r.2599@gmail.com>
2022-05-31 16:37:27 -0500

    fix: :bug: Solucionado bug de borrado de usuario

--- resources/views/usuarios/consultarUsuario.blade.php
+++ resources/views/usuarios/consultarUsuario.blade.php
@@ -29,16 +29,16 @@
                     <tbody>
                         <tr class="border-y hover:bg-gray-50">
                             <td class="p-4">
-                              {{$usuario['name']}}
+                                {{$usuario['name']}}
                             </td>
                             <td class="p-4">
-                              {{$usuario['last_name']}}
+                                {{$usuario['last_name']}}
                             </td>
                             <td class="p-4">
-                              {{$usuario['role']}}
+                                {{$usuario['role']}}
                             </td>
                             <td class="p-4">
-                              {{$usuario['email']}}
+                                {{$usuario['email']}}
                             </td>
                             <td class="p-4">
                                 <div class="group inline-block relative">
@@ -57,38 +57,45 @@
                                                 Editar</a>
                                         </li>
                                         <li class="z-10">
-                                            <a class="z-10 bg-white border hover:bg-gray-200 font-light text-lg py-2 px-4 block whitespace-no-wrap" @click=" isModalOpen = true ">
+                                            <a class="z-10 bg-white border hover:bg-gray-200 font-light text-lg py-2 px-4 block whitespace-no-wrap"
+                                                @click=" isModalOpen = true ">
                                                 Elminiar
                                             </a>
                                             {{-- <a class="z-10 bg-white border hover:bg-gray-200 font-light text-lg py-2 px-4 block whitespace-no-wrap">
                                                 <form action="/usuarios/{{$usuario['id']}}" method="post">
-                                                    <input class="z-10 bg-white border hover:bg-gray-200 font-light text-lg py-2 px-4 block whitespace-no-wrap" type="submit" value="Eliminar"  onclick="return confirm('¿Eliminar Usuario?')" @click=" isModalOpen = true "/>
-                                                    <input type="hidden" name="_method" value="delete" />
-                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                </form>
+                                            <input
+                                                class="z-10 bg-white border hover:bg-gray-200 font-light text-lg py-2 px-4 block whitespace-no-wrap"
+                                                type="submit" value="Eliminar"
+                                                onclick="return confirm('¿Eliminar Usuario?')"
+                                                @click=" isModalOpen = true " />
+                                            <input type="hidden" name="_method" value="delete" />
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                            </form>
                                             </a> --}}
-                                            
+
                                         </li>
                                     </ul>
                                 </div>
                             </td>
-                            <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center z-50" style="background-color: rgba(0,0,0,.5);" x-show="isModalOpen">
-                                <div class="text-left bg-white h-auto p-4 md:max-w-xl md:p-6 lg:p-8 shadow-xl rounded mx-2 md:mx-0" @click.away="isModalOpen = false">
-                                    <h2 class="text-2xl">¿Seguro que quieres borrar beneficiario?</h2>
-                                    <div class="flex flex-row justify-end space-x-4 mt-8">
 
-                                        <form action="{{url('/usuarios/{{$usuario['id']}} . '/delete')}}" method="post">
-                                            @csrf
-                                            <button class="bg-blue-appac text-white px-4 py-2 rounded no-outline focus:shadow-outline select-none" @click="isModalOpen = false" type="submit">Aceptar</button>
-                                        </form>
-
-                                        <button class="bg-slate-400 text-white px-4 py-2 rounded no-outline focus:shadow-outline select-none" @click="isModalOpen = false">Cancelar</button>
-                                    </div>
-                                </div>
-                            </div>
                         </tr>
                     </tbody>
                 </table>
+            </div>
+        </div>
+    </div>
+
+    <div x-cloak class="absolute top-0 left-0 w-full h-full flex items-center justify-center z-50" style="background-color: rgba(0,0,0,.5);" x-show="isModalOpen" >
+        <div x-cloak class="text-left bg-white h-auto p-4 md:max-w-xl md:p-6 lg:p-8 shadow-xl rounded mx-2 md:mx-0" @click.away="isModalOpen = false">
+            <h2 class="text-2xl">¿Seguro que quieres borrar este usuario?</h2>
+            <div class="flex flex-row justify-end space-x-4 mt-8">
+
+                <form action="{{url('/usuarios/'. $usuario['id'] . '/delete')}}" method="post">
+                    @csrf
+                    <button class="bg-blue-appac text-white px-4 py-2 rounded no-outline focus:shadow-outline select-none" @click="isModalOpen = false" type="submit">Aceptar</button>
+                </form>
+
+                <button class="bg-slate-400 text-white px-4 py-2 rounded no-outline focus:shadow-outline select-none" @click="isModalOpen = false">Cancelar</button>
             </div>
         </div>
     </div>
@@ -96,4 +103,3 @@
 {{-- {{die($beneficiary)}} --}}
 @endif
 @endsection
-
