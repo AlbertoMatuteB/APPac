331f517c57fa72a8d8c8903decb22b49700c6c97
Sebastian Resendiz <sebas.r.2599@gmail.com>
2022-06-03 01:27:17 -0500

    Estructura para creacion del pdf

---
+++ app/Http/Controllers/Evaluation/IndexPDFEvaluation.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Evaluation;
+
+use App\Http\Controllers\Controller;
+use App\Models\Activities;
+use App\Models\AnswerEvaluation;
+use App\Models\Areas;
+use App\Models\Evaluation;
+use Illuminate\Http\Request;
+
+class IndexPDFEvaluation extends Controller
+{
+    public function __invoke($id)
+    {
+        $evaluation = Evaluation::where('id', $id)->first();
+        $areas = Areas::all();
+        $activities = Activities::all();
+        $last_eval = Evaluation::where('beneficiary_id', $evaluation->beneficiary_id)->whereNot('id', $id)->orderBy('id', 'DESC')->first();
+        if ($last_eval != null) {
+            $prev_answers = AnswerEvaluation::where('evaluation_id', $last_eval->id)->get();
+        } else {
+            $prev_answers = [];
+        }
+
+        return view('Evaluation.eval', ['areas' => $areas, 'activities' =>  $activities, 'evaluation' => $evaluation, 'answers' => $prev_answers]);
+    }
+}
