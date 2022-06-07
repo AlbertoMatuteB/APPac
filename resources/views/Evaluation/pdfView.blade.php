8011c9c4334fdef8ff06db9a531e43bcf6022360
Sebastian Resendiz <sebas.r.2599@gmail.com>
2022-06-07 04:16:40 -0500

    feat: :sparkles: Vista del pdf ahora muestra el grafico general

--- resources/views/Evaluation/pdfView.blade.php
+++ resources/views/Evaluation/pdfView.blade.php
@@ -6,8 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>View pdf</title>
+    <!-- Scripts -->
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('chart.js/chart.js') }}"></script>
+    {{-- Styles --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <!-- Fonts -->
@@ -78,7 +81,9 @@
                 </tr>
             </tbody>
         </table>
-        <div>aqui va el grafico</div>
+        <div class="flex items-center justify-center my-4 w-full">
+            <div  class="w-3/5" ><canvas id="mainChart"></canvas></div>
+        </div>
 
           
         @foreach ($filteredAnswers as $area)
@@ -140,69 +145,111 @@
             </table>
         </div>
         @endforeach
-        
-        
-        {{-- @foreach ($areas as $area)
-
-        <div>
-            <h1 class="font-bold py-4">{{$area->name}}</h1>
-            <table class="table-auto w-full">
-                <thead class="">
-                    <tr class="bg-transparent">
-                        <th class="text-left p-4 font-medium">
-                            Actividad
-                        </th>
-                        <th class="text-center p-4 font-medium">
-                            Lo logra
-                        </th>
-                        <th class="text-center p-4 font-medium">
-                            En proceso
-                        </th>
-                        <th class="text-center p-4 font-medium">
-                            No lo logra
-                        </th>
-                        <th class="text-left px-4 font-medium">
-                            Comentario
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($activities as $activity)
-                    @if($activity->area_id == $area->id)
-                    <tr class="border-y hover:bg-gray-50">
-                        <td class="p-4 text-left">
-                            {{$activity->name}}
-                        </td>
-                        <td class="p-4 text-center">
-                            <input type="radio" name="{{$activity->id}}" value="3" disabled class="" @if ( $answers
-                                !=null) @foreach ($answers as $answer) @if ($answer->activity_id ==
-                            $activity->id && $answer->answer == 3) checked @endif @endforeach @endif>
-                        </td>
-                        <td class="p-4 text-center">
-                            <input type="radio" name="{{$activity->id}}" value="2" disabled class="" @if ( $answers
-                                !=null) @foreach ($answers as $answer) @if ($answer->activity_id ==
-                            $activity->id && $answer->answer == 2) checked @endif @endforeach @endif>
-                        </td>
-                        <td class="p-4 text-center">
-                            <input type="radio" name="{{$activity->id}}" value="1" disabled class="" @if ( $answers
-                                !=null) @foreach ($answers as $answer) @if ($answer->activity_id ==
-                            $activity->id && $answer->answer == 1) checked @endif @endforeach @endif>
-                        </td>
-                        <td class="px-4 h-full w-auto">
-                            <textarea rows="2" wrap="hard" name="{{'comment-'.$activity->id}}"
-                                class="min-h-full hover:bg-gray-50 outline-none border-none focus:border-indigo-300 w-full"
-                                disabled>@foreach($answers as $answer)@if($answer->activity_id==$activity->id){{$answer->comments}} @endif @endforeach
-                            </textarea>
-                        </td>
-                    </tr>
-                    @endif
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        @endforeach --}}
     </div>
     {{-- {{dd($filteredAnswers)}}       --}}
+    <script> 
+    
+
+        let rLabels = { 0 : 'No lo logra',
+        1 : 'No lo logra', 2 : 'En proceso', 3 : 'Lo logra',
+        }
+        
+        let ctx0 = document.getElementById('mainChart');
+        let myChart0 = new Chart(ctx0, {
+        type: 'polarArea',
+        data: {
+            
+            labels: [@foreach($answers as $answer) @if($answer -> answer > 1 || $answer -> comments != null) "{{$answer ->  activity -> name}}" , @endif  @endforeach],
+            datasets: [{
+                label: 'Actividad',
+                
+                data: [@foreach ($answers as $answer) @if($answer -> answer > 1 || $answer -> comments != null) {{$answer -> answer}}, @endif  @endforeach],
+                
+                backgroundColor: [
+                    'rgba(255, 99, 132)',
+                    'rgba(75, 192, 192)',
+                    'rgb(255, 205, 86)',
+                    'rgb(201, 203, 207)',
+                    'rgb(54, 162, 235)',
+                    'rgb(25, 25, 112)',
+                    'rgb(137, 207, 240)',
+                    'rgb(8, 143, 143)',
+                    'rgb(100, 149, 237)',
+                    'rgb(204, 204, 255)',
+                    'rgb(225, 193, 110)',
+                    'rgb(205, 127, 50)',
+                    'rgb(165, 42, 42)',
+                    'rgb(128, 128, 0)',
+                ],
+                borderColor: [
+                    'rgba(0, 0, 0, 1)',
+                    'rgb(255, 99, 132)',
+                    'rgb(75, 192, 192)',
+                    'rgb(255, 205, 86)',
+                    'rgb(201, 203, 207)',
+                    'rgb(54, 162, 235)'
+                    
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Resumen de progreso'
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function(context) {
+                            let label = context.label || '';
+
+                            if (label) {
+                                label += ': ';
+                            }
+                            if (context.parsed.r !== null) {
+                                if(context.parsed.r === 3){
+                                    label += "Lo logra"; 
+                                }
+                                if(context.parsed.r === 2){
+                                    label += "En proceso"; 
+                                }
+                                if(context.parsed.r === 1){
+                                    label += "No lo logra"; 
+                                }
+                            }
+                            return label;
+                        }
+                    }
+                }
+                
+            },
+            scales: {
+            
+                rAxes: {
+                    beginAtZero: true,
+                    grid: {
+                        lineWidth: 4,
+                        color: 'black',
+                        z: 1,
+                    },
+                    
+                ticks: {
+                    callback: function(value, index, values) {
+                        // for a value (tick) equals to 8
+                        return rLabels[value];
+                        // 'junior-dev' will be returned instead and displayed on your chart
+                    },
+                    z: 1,
+                    color: 'black',
+                }
+            }
+            }
+        }
+
+        
+    });
+    </script>
 </body>
 
 </html>
